97b996dfc711af47c20a77e15ca0aada154df271
mogakamo <mogaka.amo254@gmail.com>
2023-02-05 22:53:07 +0300

    [commit]: about section done!

---
+++ src/components/Landing/About.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="bg-gray-50 items-center flex justify-center">
+      <p className="bg-gray-50 text-center leading-loose w-auto text-2xl tracking-wide p-20">
+        Are you tired of the <span className="text-primary">hassle</span> and{" "}
+        <span className="text-primary">inconvenience</span> of traditional rent
+        payment methods? Are you looking for an easier, more efficient way to
+        manage your property?{" "}
+        <span className="text-primary">Look no further!</span> Our innovative
+        technology is here to revolutionize the way you{" "}
+        <span className="text-primary">pay rent</span> and <span className="text-primary">manage your property</span>.
+      </p>
+    </div>
+  );
+};
+
+export default About;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import { Hero } from '../components'
-import { MainLayout } from '../layouts'
+import React from "react";
+import { About, Features, Hero } from "../components";
+import { MainLayout } from "../layouts";
 
 const HomePage = () => {
   return (
     <MainLayout>
       <Hero />
+      <About />
+      <Features />
     </MainLayout>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
