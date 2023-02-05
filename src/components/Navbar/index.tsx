a8f85b15852b5bf9f499bf59599433c3bc105cfc
mogakamo <mogaka.amo254@gmail.com>
2023-02-05 22:15:39 +0300

    [commit]: NavBar done

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Logo from "../../assets/logo.png"
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <div className="flex justify-between p-5 shadow-lg">
+      {/* Logo */}
+      <div>
+        <img src={Logo} alt="logo" className="h-10" />
+      </div>
+      <div className="space-x-3">
+        <button className="bg-primary/80 text-white px-4 py-2 font-semibold rounded-lg tracking-wide leading">
+          Landlords
+        </button>
+        <button className="bg-primary/80 text-white px-4 py-2 font-semibold rounded-lg tracking-wide leading">
+          Tenants
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
