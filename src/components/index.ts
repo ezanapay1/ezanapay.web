28e2b21f8fad0c095a8454392fde5e0bbc3b5ba0
mogakamo <mogaka.amo254@gmail.com>
2023-03-14 12:09:41 +0300

    [feat]: updated the NavBar

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 export { default as App } from "./App";
-export { default as NavBar } from "./Navbar";
+export { NavBar } from "./Navbar";
 export { default as Footer } from "./Footer";
 export { default as Hero } from "./Hero";
 export { default as About } from "./Landing/About";
