5d2a4b2594153a1617b0939ef165108a27f7d547
mogakamo <mogaka.amo254@gmail.com>
2023-04-04 15:30:47 +0300

    [commit]: setting up the project

--- src/main.tsx
+++ src/main.tsx
@@ -1,89 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from './components';
-import './styles/index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Root from './routes/Home';
-import {
-	AuthPage,
-	Billing,
-	Homepage,
-	LandlordProfile,
-	Login,
-	PropertyListing,
-	Registration,
-	TenantsListing,
-	Wallet,
-	VerifyEmail,
-} from './routes';
-import { ErrorPage } from './error';
-import Dashboard from './routes/Landlords/Dashboard';
-import { Provider } from 'react-redux';
-
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { MantineProvider } from '@mantine/core';
-import { store } from './redux';
-
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <Homepage />,
-		errorElement: <ErrorPage />,
-	},
-	{
-		path: '/verify-email',
-		element: <VerifyEmail />,
-	},
-	{
-		path: '/login',
-		element: <Login />,
-	},
-	{
-		path: '/register',
-		element: <Registration />,
-	},
-	{
-		path: '/dashboard',
-		element: <Dashboard />,
-	},
-	{
-		path: '/properties',
-		element: <PropertyListing />,
-	},
-	{
-		path: '/landlord/profile',
-		element: <LandlordProfile />,
-	},
-	{
-		path: '/tenants',
-		element: <TenantsListing />,
-	},
-	{
-		path: '/billing',
-		element: <Billing />,
-	},
-	{
-		path: '/wallet',
-		element: <Wallet />,
-	},
-]);
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<MantineProvider
-				theme={
-					{
-						// colors: {
-						//   brand: ['#FFC107', '#FFC107', '#FFC107', '#FFC107', '#FFC107']
-						// },
-						// primaryColor: "brand",
-					}
-				}>
-				<RouterProvider router={router} />
-				<ToastContainer />
-			</MantineProvider>
-		</Provider>
-	</React.StrictMode>
-);
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,28 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
+import { dependencies } from "./package.json";
+
+function renderChunks(deps: Record<string, string>) {
+  let chunks = {};
+
+  Object.keys(deps).forEach((key) => {
+    if (["react", "react-router-dom", "react-dom"].includes(key)) return;
+
+    chunks[key] = [key];
+  });
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-router-dom", "react-dom"],
+          ...renderChunks(dependencies),
+        },
       },
     },
   },
