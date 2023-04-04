0aa4e160704017259327a405777f17e6bb4869b5
mogakamo <mogaka.amo254@gmail.com>
2023-04-04 15:51:59 +0300

    [config]: base url

---
+++ src/config/index.ts
@@ -0,0 +1,2 @@
+export const API_URL = process.env.API_URL as string;
+export const JWT_SECRET = process.env.JWT_SECRET as string;
--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { dependencies } from "./package.json";
+// import { dependencies } from "./package.json";
 
-function renderChunks(deps: Record<string, string>) {
-  let chunks = {};
+// function renderChunks(deps: Record<string, string>) {
+//   let chunks = {};
 
-  Object.keys(deps).forEach((key) => {
-    if (["react", "react-router-dom", "react-dom"].includes(key)) return;
+//   Object.keys(deps).forEach((key) => {
+//     if (["react", "react-router-dom", "react-dom"].includes(key)) return;
 
-    chunks[key] = [key];
-  });
-}
+//     chunks[key] = [key];
+//   });
+// }
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-router-dom", "react-dom"],
-          ...renderChunks(dependencies),
-        },
-      },
-    },
-  },
+  // build: {
+  //   sourcemap: false,
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks: {
+  //         vendor: ["react", "react-router-dom", "react-dom"],
+  //         ...renderChunks(dependencies),
+  //       },
+  //     },
+  //   },
+  // },
 });
